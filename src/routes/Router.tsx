d91d40ed102625adbb97d2bdcac7d32945e466b2
GUSGOMESZ <gomesgustavo1504@gmail.com>
2025-10-03 13:10:00 -0300

    feat: student page route on router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router";
 import SignIn from "../pages/signin/SignIn";
+import { Student } from "../pages/student/Student";
 
 export const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <SignIn />,
+    },
+    {
+      path: "/students",
+      element: <Student />,
     },
   ]);
 
